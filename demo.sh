95bd2f37110d016f4db1c3e46ce758d97f606ba1
linliangtao <1428129601@qq.com>
2018-06-07 13:33:37 +0800

    demo.sh

--- demo.sh
+++ demo.sh
@@ -5,7 +5,9 @@ else
   mkdir $1
   cd $1
   mkdir css js
-  touch index.html css/style.css js/main.js
+  echo "<!DOCTYPE>\n<title>Hello</title>\n<h1>Hi</h1>" > index.html
+  echo "h1{color: red;}" > css/style.css
+  echo "var string = \"Hello World\"\nalert(string)" > js/main.js
   echo 'success'
   exit 0
 fi
